8388c5b057783d9cb104ef2d35161ada581adce6
TeresaReLo <joannatere@yahoo.com.mx>
2024-06-18 10:11:14 -0600

    files

---
+++ Verif/fv_top_module.sv
@@ -0,0 +1,20 @@
+module fv_top_module#(
+
+);
+
+
+
+/////// Assumptionss//////////////////////
+  
+
+/////// Assertions //////////////////////
+   
+   
+/////// Cover properties/////////////////
+        
+
+
+endmodule
+
+
+bind top_module fv_top_module fv_top_module_inst(.*);
