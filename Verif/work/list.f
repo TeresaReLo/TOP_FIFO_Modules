5bac24c0e9d311e05ce3604da00ae18271a99301
ana-pgodoy <anna.pgm28@gmail.com>
2024-06-21 17:43:28 -0600

    [Verif/work] vivado infrastr

---
+++ Verif/work/list.f
@@ -0,0 +1,15 @@
+#RTL
+    ../../Env/fifo_defines_pkg.sv
+    ../../RTL/funct_generator_adder.sv
+    ../../RTL/funct_generator_multi.sv
+    ../../RTL/funct_generator_register.sv
+    ../../RTL/funct_generator_lut.sv
+    ../../RTL/funct_generator_mux.sv
+    ../../RTL/funct_generator_fsm.sv 
+    ../../RTL/funct_generator.sv
+    ../../RTL/spi_rtl.sv
+    ../../RTL/fifo_rtl.sv
+    ../../RTL/top_module.sv
+
+#verification
+    ../tb/tb.sv
