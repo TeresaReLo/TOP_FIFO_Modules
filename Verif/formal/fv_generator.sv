061df2dfe1850a42f81b32e2c28a626f7e52eca8
TeresaReLo <joannatere@yahoo.com.mx>
2024-06-27 11:05:49 -0600

    generator_fv

--- Verif/formal/fv_generator.sv
+++ Verif/formal/fv_generator.sv
@@ -52,10 +52,11 @@ module fv_generator(
 	
 	// 3) The property assures that when enh is low and clrh is low, the output data_o remains unchanged.
 	data_o_stability_when_disabled: assert property (@(posedge clk) disable iff (rst) (!enh && !clrh) |=> (data_o == $past(data_o)))
-	$info("Assetion pass data_o_stability_when_disabled:"); else $error(" Asserion fail data_o_stability_when_disabled:");
+	$info("Assetion pass data_o_stability_when_disabled"); else $error(" Asserion fail data_o_stability_when_disabled");
 	
 	// 4) The property assures that the adder adds 1 to the current addess to produce the next addess when enh is high.
-	a_next_address_is_addr_plus_1: assert property (@(posedge clk) disable iff (rst) (enh && !clrh) |=> (addr_temp == addr + 1));
+	addr_increment1_when_enh: assert property (@(posedge clk) disable iff (rst) (enh && !clrh) |=> (addr_temp == addr + 1))
+	$info("Assetion pass addr_increment1_when_enh"); else $error(" Asserion fail addr_increment1_when_enh");
 
 
 /*************************************************************************** cover ***************************************************************************/
