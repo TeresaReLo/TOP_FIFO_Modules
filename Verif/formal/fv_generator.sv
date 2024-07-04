e90c06a3ada17748c5a5290c12e3d8deef236d3d
TeresaReLo <joannatere@yahoo.com.mx>
2024-07-04 13:23:12 -0600

    funct_gen_ properties

--- Verif/formal/fv_generator.sv
+++ Verif/formal/fv_generator.sv
@@ -120,16 +120,32 @@ module fv_funct_generator_fsm (
 	idle_stable: assert property (@(posedge clk) disable iff (rst) ((state_f == IDLE) && (((!enh_conf_i) && en_low_i) || rst)) |-> (next_f == IDLE))  $info("Assetion pass idle_stable");
 	else $error(" Asserion fail idle_stable");
 
+	// 4)	This property assures state transition from CONFIG to GEN when enh_conf_i and en_low_i are not active.
+	whenconfig_next_gen: assert property (@(posedge clk) disable iff (rst) (state_f == CONFIG && (!enh_conf_i) && (!en_low_i)) |-> (next_f == GEN))  $info("Assetion pass whenconfig_next_gen");
+	else $error(" Asserion fail whenconfig_next_gen");
+
+	// 5)	This property assures state transition from CONFIG to IDLE when enh_conf_i and en_low_i are not asserted 
+	whenconfig_next_idle: assert property (@(posedge clk) disable iff (rst) (state_f == CONFIG && (!enh_conf_i) && en_low_i) |-> (next_f == IDLE))  $info("Assetion pass whenconfig_next_idle");
+	else $error(" Asserion fail whenconfig_next_idle");
+
+	// 6)	This property assures state transition from GEN to CONFI when enh_conf_i is asserted.
+	whengen_next_confi: assert property (@(posedge clk) disable iff (rst) (state_f == GEN && enh_conf_i) |-> (next_f == CONFI))  $info("Assetion pass whengen_next_confi");
+	else $error(" Asserion fail whengen_next_confi");
+
+	// 7)	This property assures state transition from GEN to IDLE when enh_conf_i and en_low_i are not asserted.
+	whengen_next_idle: assert property (@(posedge clk) disable iff (rst) (state_f == GEN && (!enh_conf_i) && en_low_i) |-> (next_f == IDLE) )  $info("Assetion pass whengen_next_idle");
+	else $error(" Asserion fail whengen_next_idle");
+
 	// 8) This property assures clrh_addr_fsm should be high in IDLE or CONFI states
-	clrh_addr_fsm_when_idle_or_confi: assert property (@(posedge clk) disable iff (rst) (state == IDLE || state == CONFI) |-> clrh_addr_fsm) $info("Assetion pass clrh_addr_fsm_when_idle_or_confi");
+	clrh_addr_fsm_when_idle_or_confi: assert property (@(posedge clk) disable iff (rst) (state == IDLE || state == CONFI) |=> clrh_addr_fsm) $info("Assetion pass clrh_addr_fsm_when_idle_or_confi");
 	else $error(" Asserion fail clrh_addr_fsm_when_idle_or_confi");
 
 	// 9) This property assures enh_config_fsm should be high in CONFI state
-	 enh_config_fsm_active_when_confi: assert property (@(posedge clk) disable iff (rst) (state == CONFI) |-> enh_config_fsm) $info("Assetion pass  enh_config_fsm_active_when_confi");
+	 enh_config_fsm_active_when_confi: assert property (@(posedge clk) disable iff (rst) (state == CONFI) |=> enh_config_fsm) $info("Assetion pass  enh_config_fsm_active_when_confi");
 	else $error(" Asserion fail  enh_config_fsm_active_when_confi");
 
 	// 10) This property assures enh_gen_fsm should be high in GEN state
-	 enh_gen_fsm_active_when_gen: assert property (@(posedge clk) disable iff (rst) (state == GEN) |-> enh_gen_fsm) $info("Assetion pass  enh_gen_fsm_active_when_gen");
+	 enh_gen_fsm_active_when_gen: assert property (@(posedge clk) disable iff (rst) (state == GEN) |=> enh_gen_fsm) $info("Assetion pass  enh_gen_fsm_active_when_gen");
 	else $error(" Asserion fail  enh_gen_fsm_active_when_gen");
 
 ///////////////////////////////////////////////////// Covers /////////////////////////////////////////////////////
