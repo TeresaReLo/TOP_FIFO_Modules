9399ebd0b2a51bab9dbf09ae9bbe2b394865776b
TeresaReLo <joannatere@yahoo.com.mx>
2024-07-15 13:54:12 -0600

    properties corrections

--- RTL/spi_rtl.sv
+++ RTL/spi_rtl.sv
@@ -1,4 +1,3 @@
-
 module spi_serializer #(
     parameter DATAWIDTH = 32,
     parameter BITCOUNTERWIDTH =  $clog2(DATAWIDTH)) 
@@ -46,53 +45,57 @@ module spi_serializer #(
     	end
   
  
-	always_ff @(posedge clk or posedge rst) begin
-      	        
-                if (rst || empty) begin
-        		state <= IDLE;
+ 	always_ff@(posedge clk or posedge rst) begin
+     		if(rst) state <= IDLE;                                            
+       		else state <= next_state;
+  	end 
+ 
+	always_ff @(posedge clk or posedge rst) begin  
+ 		if (rst) begin
         		shift_reg <= '0;
         		bit_counter <= '0;
         		done <= 1'b0;
             		mosi <= 1'b0;
             		sclk_enable <= 1'b0;
-                        read_en <= 1'b0;
-        	end else begin
-            	state <= next_state;
-                read_en <= 1'b0;
-            	case (state)
-              	IDLE: begin
-			shift_reg <= '0;
-            		bit_counter <= '0;	
-           		done <= 1'b0;
-            		mosi <= 1'b0;
+                	read_en <= 1'b0;
+        		end else begin
+                	read_en <= 1'b0;
+            		
+			case (next_state)
+              		IDLE: begin
+				shift_reg <= '0;
+            			bit_counter <= '0;	
+           			done <= 1'b0;
+            			mosi <= 1'b0;
             		sclk_enable <= 1'b0;
-                end
-                LOAD: begin
-                    shift_reg <= read_data;
-                    read_en <= 1'b1;   
-                    bit_counter <= DATAWIDTH;
-                    sclk_enable <= 1'b1;  // Enable sclk during SHIFT state
-                end
-                SHIFT: begin
-                    if (sclk && clk_div) begin  // Shift data on the negative edge of sclk
-			mosi <= shift_reg[DATAWIDTH-1];
-                        shift_reg <= shift_reg << 1; //1 Shift Left
-                        bit_counter <= bit_counter - 1;
-                    end
-                end
-                COMPLETE: begin
-                    done <= 1'b1;
-                    sclk_enable <= 1'b0;  // Disable sclk in COMPLETE state
-                end
-            endcase
-        end
-    end
+                	end
+                	LOAD: begin
+                    		shift_reg <= read_data;
+                    		read_en <= 1'b1;
+                    		bit_counter <= DATAWIDTH;
+                	end
+                	SHIFT: begin
+                   		sclk_enable <= 1'b1;  // Enable sclk during SHIFT state
+              			if (sclk && clk_div) begin //Shift data on the - edge
+					mosi <= shift_reg[DATAWIDTH-1];
+                        		shift_reg <= shift_reg << 1; //1 Shift Left
+                        		bit_counter <= bit_counter - 1;
+                    		end
+                	end
+                	COMPLETE: begin
+                    		done <= 1'b1;
+                    		sclk_enable <= 1'b0;  // Disable sclk in COMPLETE state
+                	end
+            		endcase
+        	end
+	end
 
-    	always_comb begin
+	always_comb begin
         	next_state = state;
         	case (state)
             	IDLE: begin //00
-              		if ((full || !empty)&& !done) next_state = LOAD;  //start when full or not empty and done is not active.
+                	if (empty) next_state = IDLE;
+              		else if ((full || !empty)&& !done) next_state = LOAD;  //start when full or not empty and done is not active.
             	end
             	LOAD: begin //01
               		next_state = SHIFT;
