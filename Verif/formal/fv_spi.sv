b8cb143a24219c20d03709a75ef5f980681c8962
ana-pgodoy <anna.pgm28@gmail.com>
2024-07-18 10:43:15 -0600

    [formal] Properties added

--- Verif/formal/fv_spi.sv
+++ Verif/formal/fv_spi.sv
@@ -34,13 +34,26 @@ module fv_spi(
     typedef enum logic [1:0] {IDLE, LOAD, SHIFT, COMPLETE} state_type;
     state_type state_fv, next_state_fv;
     bit flag_t1;
+    bit [`DATA_WIDTH-1:0] data_read, data_serial;
 
-    always_ff @(posedge clk, posedge rst)begin
+
+     always_ff @(posedge clk, posedge rst)begin
         if(rst) flag_t1 <= 1'b0;  
         else flag_t1 <= 1'b1;
     end
 
-    ///// Para utilizar los nombres de los estados en las aserciones unicamente ///////////////
+    always_comb begin
+        if(rst) data_serial = '0;
+        else 
+        if ((state_fv == SHIFT) && (!sclk) && (!clk_div)) data_serial[bit_counter] = mosi;
+        else if(state_fv == IDLE) data_serial = '0;
+    end
+
+    always_comb begin
+        if(read_en) data_read = read_data;
+    end
+
+    //////////// Para utilizar los nombres de los estados en las aserciones unicamente ///////////////
     always_comb begin
         case(state) 
             0: state_fv = IDLE;
@@ -58,7 +71,7 @@ module fv_spi(
             3: next_state_fv = COMPLETE;
         endcase
     end
-    ////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
 
     /*************************************************************************** assume ***************************************************************************/
     
@@ -86,18 +99,18 @@ module fv_spi(
 	//else $error("empty enable assertion failed at time %t", $time);
 
     // 4) Done output must be activated two clock cycles after bit counter is equal to zero 
-	done_after_bitcounter_zero_assert : assert property (@(posedge clk) disable iff (rst) ((bit_counter == '0) && (state_fv == SHIFT)) |=> (done));
+    done_after_bitcounter_zero_assert : assert property (@(posedge clk) disable iff (rst) ((bit_counter == '0) && (state_fv == SHIFT)) |=> (done));
         //$info("Done after bit counter = 0 assertiion passed");
         //else $error("Done after bit counter = 0 assertion failed at time %t", $time);
 
     // 5) Done output must to be activated one cycle after the COMPLETE state 
-	complete_state_then_done_assert : assert property (@(posedge clk) disable iff (rst) (state_fv == COMPLETE) |-> (done));
+    complete_state_then_done_assert : assert property (@(posedge clk) disable iff (rst) (state_fv == COMPLETE) |-> (done));
         //$info("Done after complete assertiion passed");
         //else $error("Done after complete assertion failed at time %t", $time);
     
     // 6) If done is activated, the state one clock cycle before must have been COMPLETE
     done_only_complete_state_assert : assert property (@(posedge clk) disable iff (rst) (done) |-> (state_fv == COMPLETE));
-	    //$info("Done in complete state assertiion passed");
+	//$info("Done in complete state assertiion passed");
         //else $error("Done after complete assertion failed at time %t", $time);
 
     // 7) Done output must remain assert for only 1 clock cycle since must be activated in the COMPLETE state and this state returns to IDLE in the next cycle without any condition.
@@ -176,7 +189,7 @@ module fv_spi(
         //else $error("State SHIFT to COMPLETE assertion failed at time %t", $time);
     
     // 22) read_en must be asserted one clock cycle after LOAD state 
-	    load_state_then_read_en_assert : assert property (@(posedge clk) disable iff (rst) (state_fv == LOAD) |-> (read_en) );
+    load_state_then_read_en_assert : assert property (@(posedge clk) disable iff (rst) (state_fv == LOAD) |-> (read_en) );
         //$info("load_state_then_read_en assertiion passed");
         //else $error("load_state_then_read_en assertion failed at time %t", $time);
 
@@ -216,7 +229,7 @@ module fv_spi(
         //else $error("clk_div toggle assertion failed at time %t", $time);
 
     // 30) sclk must to toggle when sclk_enable=1 and negedge of clk_div 
-	    sclk_toggle_enable_assert : assert property (@(posedge clk) disable iff (rst) ((bit_counter != '0) && (sclk_enable) && ($fell(clk_div))) |-> ##2 ($changed(sclk)));
+    sclk_toggle_enable_assert : assert property (@(posedge clk) disable iff (rst) ((bit_counter != '0) && (sclk_enable) && ($fell(clk_div))) |-> ##2 ($changed(sclk)));
         //$info("sclk_toggle_enable assertion passed");
         //else $error("sclk_toggle_enable assertion failed at time %t", $time);
 
@@ -230,10 +243,22 @@ module fv_spi(
         //$info("mosi_bit_shift_reg assertion passed");
         //else $error("mosi_bit_shift_reg assertion failed at time %t with mosi = %b shift_reg[msb] = %b ", $time, mosi, shift_reg[`DATA_WIDTH-1]);
     
-    // 33)     
+    // 33) Shift-left operation is equivalent to multiplying by 2, therefore when shifting the register we must obtain the same register multiplied by 2     
     shift_left_mult_2_assert : assert property (@(posedge clk) disable iff (rst) ((state_fv == SHIFT) && ($fell(sclk))) |-> (shift_reg == (($past(shift_reg))*2) ) );
         //$info("shift_left assertion passed");
         //else $error("shift_left assertion failed at time %t", $time);
+    
+    // 34) When bit_counter is at zero it means that there are no more bits to go through so the register must be equal to zero
+    shift_reg_zero_bitcounter_zero_assert : assert property (@(posedge clk) disable iff (rst) ((state_fv == SHIFT) && (bit_counter == '0)) |-> (shift_reg == '0));
+    
+    // 35) When state is IDLE or COMPLETE shift_reg must to be stable
+    shift_reg_stable_idle_or_complet_assert : assert property (@(posedge clk) disable iff (rst) ((state_fv == IDLE) && (state_fv == COMPLETE)) |-> ($stable(shift_reg)));
+    
+    // 36) Mosi signal must be equal to zero when state is IDLE or LOAD
+    mosi_zero_idle_load_states_assert : assert property (@(posedge clk) disable iff (rst) ((state_fv == IDLE) && (state_fv == LOAD)) |-> (mosi == 1'b0));
+    
+    // 37) Data saved in shift_reg during LOAD state must be equal to the mosi sequence in SHIFT state
+    read_data_equal_mosi_sequence_assert : assert property (@(posedge clk) disable iff (rst) (done) |-> (data_serial == data_read));
 
     /*************************************************************************** cover ***************************************************************************/
     
@@ -259,4 +284,4 @@ module fv_spi(
     sclk_enable_cover : cover property (@(posedge clk) (sclk_enable));
 endmodule
 
-bind spi_serializer fv_spi fv_spi_inst(.*); 
+//bind spi_serializer fv_spi fv_spi_inst(.*); 
